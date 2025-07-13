620fa152621f109529ed9449377bdae6e6a242fe
Kugeleis <kugeleis@wbrief.de>
2025-07-13 10:23:52 +0200

    fixes

---
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -0,0 +1,7 @@
+"""{{ cookiecutter.project_description }}"""
+
+__version__ = "{{ cookiecutter.version }}"
+
+from .main import main
+
+__all__ = ["main"]
---
+++ {{cookiecutter.project_slug}}/tests/test_main.py
@@ -0,0 +1,8 @@
+"""Tests for {{ cookiecutter.project_name }}."""
+
+def test_main():
+    """Test the main function."""
+    # This is a placeholder test
+    # Add your actual tests here
+    from {{ cookiecutter.project_slug }}.main import main
+    assert main() is None
